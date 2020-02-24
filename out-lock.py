7751c7ab24fb53bcc35a20b64f3424f2322bfa76
Hafizh Fadhlurrohman <hafizhfadh94@gmail.com>
2020-02-24 17:05:51 +0700

    Add

--- out-lock.py
+++ out-lock.py
@@ -1,6 +1,5 @@
 import requests
-from num2words import num2words
-from subprocess import call
+import os
 
 while True:
     URL = "http://api.wibs.sch.id//v2/dorm/post/outin.update-timestamp"
@@ -12,13 +11,11 @@ while True:
     r = requests.post(url=URL, data=data, headers={
         "Application-Token": "geSzgVahOlowulcgHEtQmu9Ybofk1lRnPFd3V5atSEu1SD1dt2"})
     response = r.json()
-    cmd_beg = 'espeak '
-    # To play back the stored .wav file and to dump the std errors to /dev/null
-    cmd_end = ' | aplay /home/pi/Desktop/Text.wav  2>/dev/null'
-    cmd_out = '--stdout > /home/pi/Desktop/Text.wav '  # To store the voice file
     if response['status']:
         print('Akses diterima ! %s' % response['message'])
-        call([cmd_beg+cmd_out+"Access_Granted"+cmd_end], shell=True)
+        file = "granted.mp3"
+        os.system("mpg123 " + file)
     else:
         print('Akses ditolak ! %s' % response['message'])
-        call([cmd_beg+cmd_out+"Access_Denied"+cmd_end], shell=True)
+        file = "denied.mp3"
+        os.system("mpg123 " + file)
