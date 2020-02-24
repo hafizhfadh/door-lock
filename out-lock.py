9312d96dfbbcc7f44a184dfa1ee8e1d39b20c947
Hafizh Fadhlurrohman <hafizhfadh94@gmail.com>
2020-02-24 16:06:16 +0700

    Add New

---
+++ out-lock.py
@@ -0,0 +1,16 @@
+import requests
+
+while True:
+    URL = "http://api.wibs.sch.id//v2/dorm/post/outin.update-timestamp"
+    rfid = input("Please insert RFID : ")
+    data = {
+        'student_rfid': rfid,
+        'type': 'out'
+    }
+    r = requests.post(url=URL, data=data, headers={
+                    "Application-Token": "geSzgVahOlowulcgHEtQmu9Ybofk1lRnPFd3V5atSEu1SD1dt2"})
+    response = r.json()
+    if response['status']:
+        print('Akses diterima ! %s' %response['message'])
+    else:
+        print('Akses ditolak ! %s' %response['message'])
