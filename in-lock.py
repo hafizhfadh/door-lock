6a0d4901a8ea9d9c15e629e3f046970b1539ec4f
Hafizh Fadhlurrohman <hafizhfadh94@gmail.com>
2020-02-25 10:10:43 +0700

    add

--- in-lock.py
+++ in-lock.py
@@ -1,7 +1,12 @@
 import requests
 import os
+import RPi.GPIO as GPIO
+import signal
+import sys
 
 while True:
+    GPIO.setmode(GPIO.BCM)
+    pin.DOOR_SENSOR_PIN = 12
     URL = "http://api.wibs.sch.id//v2/dorm/post/outin.update-timestamp"
     rfid = input("Please insert RFID : ")
     data = {
@@ -15,7 +20,11 @@ while True:
         print('Akses diterima ! %s' % response['message'])
         file = "granted.mp3"
         os.system("mpg123 " + file)
+        GPIO.output(DOOR_SENSOR_PIN, False)
+        GPIO.cleanup()
     else:
         print('Akses ditolak ! %s' % response['message'])
         file = "denied.mp3"
         os.system("mpg123 " + file)
+        GPIO.output(DOOR_SENSOR_PIN, True)
+        GPIO.cleanup()
