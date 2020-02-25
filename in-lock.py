9d33a29ae901d2c3621fbedca441baabf8caad34
Hafizh Fadhlurrohman <hafizhfadh94@gmail.com>
2020-02-25 13:19:25 +0700

    add

--- in-lock.py
+++ in-lock.py
@@ -1,33 +1,43 @@
 import requests
 import os
 import RPi.GPIO as GPIO
+import time
+
+CONTROL_PIN = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(CONTROL_PIN, GPIO.OUT)
+
+def setDoorStatus(status, response):
+    if status:
+        print('Akses diterima ! %s' % response['message'])
+        # file = "granted.mp3"
+        # os.system("mpg123 " + file)
+        GPIO.output(CONTROL_PIN, False)
+        GPIO.cleanup()
+    else:
+        print('Akses ditolak ! %s' % response['message'])
+        # file = "denied.mp3"
+        # os.system("mpg123 " + file)
+        GPIO.output(CONTROL_PIN, True)
+        GPIO.cleanup()
+
+def sendData(rfid):
+    URL = "http://api.wibs.sch.id//v2/dorm/post/outin.update-timestamp"
+    data = {
+        'student_rfid': rfid,
+        'type': 'in'
+    }
+    r = requests.post(url=URL, data=data, headers={
+            "Application-Token": "geSzgVahOlowulcgHEtQmu9Ybofk1lRnPFd3V5atSEu1SD1dt2"})
+    response = r.json()
+    setDoorStatus(status = response['status'], response = response)
 
 while True:
     try:
-        CONTROL_PIN = 12
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(CONTROL_PIN, GPIO.OUT)
+        time.sleep(10)
         GPIO.output(CONTROL_PIN, True)
-        URL = "http://api.wibs.sch.id//v2/dorm/post/outin.update-timestamp"
         rfid = input("Please insert RFID : ")
-        data = {
-            'student_rfid': rfid,
-            'type': 'in'
-        }
-        r = requests.post(url=URL, data=data, headers={
-            "Application-Token": "geSzgVahOlowulcgHEtQmu9Ybofk1lRnPFd3V5atSEu1SD1dt2"})
-        response = r.json()
-        if response['status']:
-            print('Akses diterima ! %s' % response['message'])
-            # file = "granted.mp3"
-            # os.system("mpg123 " + file)
-            GPIO.output(CONTROL_PIN, False)
-            GPIO.cleanup()
-        else:
-            print('Akses ditolak ! %s' % response['message'])
-            # file = "denied.mp3"
-            # os.system("mpg123 " + file)
-            GPIO.output(CONTROL_PIN, True)
-            GPIO.cleanup()
+        sendData(rfid = rfid)
     finally:
+        GPIO.output(CONTROL_PIN, True)
         GPIO.cleanup()
