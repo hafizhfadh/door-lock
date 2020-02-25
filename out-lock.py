b148968c1fea42f4013538e4af2204d16deafeb2
Hafizh Fadhlurrohman <hafizhfadh94@gmail.com>
2020-02-25 10:21:46 +0700

    add

--- out-lock.py
+++ out-lock.py
@@ -1,7 +1,12 @@
 import requests
 import os
+import RPi.GPIO as GPIO
 
 while True:
+    CONTROL_PIN = 12
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(CONTROL_PIN, GPIO.OUT)
+    GPIO.output(CONTROL_PIN, True)
     URL = "http://api.wibs.sch.id//v2/dorm/post/outin.update-timestamp"
     rfid = input("Please insert RFID : ")
     data = {
@@ -13,9 +18,13 @@ while True:
     response = r.json()
     if response['status']:
         print('Akses diterima ! %s' % response['message'])
-        file = "granted.mp3"
-        os.system("mpg123 " + file)
+        # file = "granted.mp3"
+        # os.system("mpg123 " + file)
+        GPIO.output(CONTROL_PIN, False)
+        GPIO.cleanup()
     else:
         print('Akses ditolak ! %s' % response['message'])
-        file = "denied.mp3"
-        os.system("mpg123 " + file)
+        # file = "denied.mp3"
+        # os.system("mpg123 " + file)
+        GPIO.output(CONTROL_PIN, True)
+        GPIO.cleanup()
